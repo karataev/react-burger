a0abe2ca3a7ee0a4f0d580fbcfa742c618caa069
karataev <vfl45es1>
2023-05-25 23:15:46 +0700

    Доработки модалок

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -2,10 +2,9 @@ import ReactDOM from "react-dom";
 import styles from './modal.module.css';
 import PropTypes from "prop-types";
 import {useEffect} from "react";
-import {CloseIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 import ModalOverlay from "./modal-overlay/modal-overlay";
 
-function Modal({title, onClose, children}) {
+function Modal({onClose, children}) {
 
   useEffect(() => {
     function onKeyDown(e) {
@@ -22,12 +21,6 @@ function Modal({title, onClose, children}) {
   const element = (
     <div className={styles.root}>
       <section className={styles.inner}>
-        <header className={styles.header}>
-          <h1 className="text text_type_main-large">{title}</h1>
-          <button className={styles.close_bt} onClick={onClose}>
-            <CloseIcon type="primary" />
-          </button>
-        </header>
         {children}
       </section>
       <ModalOverlay onClick={onClose} />
@@ -38,7 +31,6 @@ function Modal({title, onClose, children}) {
 }
 
 Modal.propTypes = {
-  title: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
 }
 
