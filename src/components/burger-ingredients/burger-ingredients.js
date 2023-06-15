dd028af84ed1f8aeea8265e55215c0d5d57f6e6b
karataev <eugene.karataev@gmail.com>
2023-06-15 16:45:17 +0700

    Активация табов при скролле

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,10 +1,11 @@
-import {useMemo} from 'react';
+import {useCallback, useMemo, useRef} from 'react';
 import styles from './burger-ingredients.module.css';
 import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
 import IngredientGroup from "./ingredient-group/ingredient-group";
 import IngredientDetails from "./ingredient-details/ingredient-details";
 import {useDispatch, useSelector} from "react-redux";
 import {GROUP_BUNS, GROUP_FILLINGS, GROUP_SAUCES} from "../../utils/constants";
+import debounce from 'lodash.debounce';
 
 import {SET_CURRENT_TAB} from "../../services/actions/ingredients";
 
@@ -17,13 +18,45 @@ function BurgerIngredients() {
 
   const dispatch = useDispatch();
 
+  const rootRef = useRef();
+  const bunsRef = useRef();
+  const saucesRef = useRef();
+  const fillingsRef = useRef();
+
   function setCurrentTab(tab) {
     dispatch({type: SET_CURRENT_TAB, tab});
   }
 
+  function getDist(ref) {
+    const rootTop = rootRef.current.getBoundingClientRect().top;
+    const refTop = ref.current.getBoundingClientRect().top;
+    return Math.abs(rootTop - refTop);
+  }
+
+  const onScroll = useCallback(() => {
+    let minDist = 100000;
+    let closestTab = '';
+    const items = [
+      {tab: GROUP_BUNS, dist: getDist(bunsRef)},
+      {tab: GROUP_SAUCES, dist: getDist(saucesRef)},
+      {tab: GROUP_FILLINGS, dist: getDist(fillingsRef)},
+    ]
+    items.forEach(item => {
+      if (item.dist < minDist) {
+        minDist = item.dist;
+        closestTab = item.tab;
+      }
+    })
+    if (currentTab !== closestTab) {
+      dispatch({type: SET_CURRENT_TAB, tab: closestTab});
+    }
+  }, [currentTab, dispatch]);
+
+  const debouncedScroll = useMemo(() => debounce(onScroll, 50), [onScroll]);
+
   return (
     <>
-      <section className={`pt-10 ${styles.root}`}>
+      <section className={`pt-10 ${styles.root}`} ref={rootRef}>
         <h1 className="text text_type_main-large">Соберите бургер</h1>
         <div className={`mt-5 ${styles.tabs}`}>
           <Tab active={currentTab === GROUP_BUNS} value={GROUP_BUNS} className={styles.tab} onClick={setCurrentTab}>
@@ -36,10 +69,10 @@ function BurgerIngredients() {
             Начинки
           </Tab>
         </div>
-        <div className={`${styles.scrollable} custom-scroll`}>
-          <IngredientGroup type={GROUP_BUNS} items={buns} />
-          <IngredientGroup type={GROUP_SAUCES} items={sauces} />
-          <IngredientGroup type={GROUP_FILLINGS} items={fillings} />
+        <div className={`${styles.scrollable} custom-scroll`} onScroll={debouncedScroll}>
+          <IngredientGroup type={GROUP_BUNS} items={buns} groupRef={bunsRef} />
+          <IngredientGroup type={GROUP_SAUCES} items={sauces} groupRef={saucesRef} />
+          <IngredientGroup type={GROUP_FILLINGS} items={fillings} groupRef={fillingsRef} />
         </div>
       </section>
       <IngredientDetails />
--- src/components/burger-ingredients/ingredient-group/ingredient-group.js
+++ src/components/burger-ingredients/ingredient-group/ingredient-group.js
@@ -2,27 +2,19 @@ import styles from "./ingredient-group.module.css";
 import PropTypes from "prop-types";
 import {ingredientType} from "../../../utils/types";
 import IngredientCard from "../ingredient-card/ingredient-card";
-import {useSelector} from "react-redux";
-import {useEffect, useMemo, useRef} from "react";
+import {useMemo} from "react";
 import {GROUP_BUNS, GROUP_SAUCES} from "../../../utils/constants";
 
-function IngredientGroup({items, type}) {
-  const titleRef = useRef();
-  const currentTab = useSelector(store => store.ingredients.currentTab);
-
+function IngredientGroup({items, type, groupRef}) {
   const title = useMemo(() => {
     if (type === GROUP_BUNS) return 'Булки';
     if (type === GROUP_SAUCES) return 'Соусы';
     return 'Начинки';
   }, [type])
 
-  useEffect(() => {
-    if (currentTab === type && titleRef.current) titleRef.current.scrollIntoView();
-  }, [currentTab, type]);
-
   return (
     <>
-      <h2 ref={titleRef} className="text text_type_main-medium pt-10">{title}</h2>
+      <h2 ref={groupRef} className="text text_type_main-medium pt-10">{title}</h2>
       <ul className={styles.cards}>
         {items.map(item => {
           return (
