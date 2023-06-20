21b492d0e7c0bd04f602414f3da5839ec3d70ecb
karataev <eugene.karataev@gmail.com>
2023-06-20 21:38:22 +0700

    Загрузка данных при старте пирложения

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,19 +2,34 @@ import HomePage from "../../pages/home/home-page";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import ProfilePage from "../../pages/profile/profile-page";
 import styles from './app.module.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {getIngredients} from "../../services/actions/ingredients";
+import Loader from "../loader/loader";
 
 function App() {
+  const dispatch = useDispatch();
+  const ingredientsLoading = useSelector(store => store.ingredients.ingredientsLoading);
+  const ingredientsError = useSelector(store => store.ingredients.ingredientsError);
+
+  useEffect(() => {
+    dispatch(getIngredients());
+  }, [dispatch]);
+
   return (
     <div className={styles.root}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-        </Routes>
-      </BrowserRouter>
+      {ingredientsLoading && <Loader />}
+      {ingredientsError && <div>Произошла ошибка</div>}
+      {!ingredientsLoading && !ingredientsError && (
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/profile" element={<ProfilePage />} />
+          </Routes>
+        </BrowserRouter>
+      )}
     </div>
   )
-
 }
 
 export default App;
